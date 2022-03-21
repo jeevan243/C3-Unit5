ee5c3c7ef202a347dbb1bf481b007918b1de8825
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-03-21 16:27:35 +0530

    product page added

--- src/Components/BookDetailsPage/BookDetailsPage.jsx
+++ src/Components/BookDetailsPage/BookDetailsPage.jsx
@@ -1,18 +1,46 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export const BookDetailsPage = () => {
+  const { id } = useParams();
+  const [book, getBook] = useState([]);
   // Get book details based on ID whenever user lands on the page
   // ID will come from route
+
+  console.log(id);
+  console.log("here");
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axios.get(`http://localhost:8080/books/${id}`).then(({ data }) => {
+      getBook(...book, data);
+      // console.log(data);
+    });
+  };
+
+  console.log(book);
+
+  let review = book.reviews;
+  console.log(review);
 
   return (
     <>
       <div className="bookContainer">
-        <h2 className="title">{"show book tile here"}</h2>
-        <img className="image" src={"show book image here"} alt="#" />
-        <div className="author">{"show book Author here"}</div>
-        <div className="description">{"show book Description here"}</div>
-        <div className="price">{"show book Price here"}</div>
-        <div className="section">{"show book section name here"}</div>
-        <div className="isbnNumber">{"show book ISBN number here"}</div>
+        <h2 className="title">{book.title}</h2>
+        <img className="image" src={book.imageUrl} alt="#" />
+        <div className="author">{book.author}</div>
+        <div className="description">{book.description}</div>
+        <div className="price">{book.price}</div>
+        <div className="section">{book.section}</div>
+        <div className="isbnNumber">{book.isbnNumber}</div>
         <ul className="reviews">
+          {/* {review.map((e) => {
+            return <li>{e}</li>;
+          })} */}
           {/* Reviews will be an array, iterate over them and create a new <li> for every review */}
         </ul>
       </div>
