fcc54ccc04c7a07dade546c3de14c6a23242fa7d
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-03-28 18:12:14 +0530

    classes added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,56 +1,82 @@
 import { BookCard } from "../BookCard/BookCard";
 import { SortAndFilterButtons } from "../SortAndFilterButtons/SortAndFilterButtons";
-
 import styled from "styled-components";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+
 export const Home = () => {
-  const [books, setgetBooks] = useState([]);
   // get all books when user lands on the page
   // populate them as mentioned below
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  const getData = () => {
-    axios.get("http://localhost:8080/books").then((res) => {
-      setgetBooks(...books, res.data);
-    });
-  };
-
   const Main = styled.div`
+    margin-top: 20px;
+    display: flex;
+    flex-wrap: wrap;
     /* Apply some responsive styling to children */
   `;
+
+  const [allbooks, setAllbooks] = useState([]);
+  useEffect(() => {
+    console.log("1");
+    axios.get("http://localhost:8080/books").then((res) => {
+      setAllbooks(res.data);
+      console.log(res.data);
+    });
+  }, []);
+  const [filterSort, setFilter] = useState({
+    parameter: "",
+    value: null,
+  });
+  const handleSort = (parameter, value) => {
+    setFilter({ parameter, value });
+    // console.log(filterSort);
+  };
 
   return (
     <div className="homeContainer">
       <h2 style={{ textAlign: "center" }}>Home</h2>
-      <SortAndFilterButtons
-        handleSort={
-          "give handleSort function to this component, that sorts books"
-        }
-      />
+      <SortAndFilterButtons handleSort={handleSort} />
 
       <Main className="mainContainer">
-        {/* 
+        {
+          allbooks
+            .sort((a, b) => {
+              if (filterSort.parameter == "title" && filterSort.value == 1) {
+                return a["title"].localeCompare(b["title"]);
+              } else if (
+                filterSort.parameter == "title" &&
+                filterSort.value == -1
+              ) {
+                return b["title"].localeCompare(a["title"]);
+              } else if (
+                filterSort.parameter == "price" &&
+                filterSort.value == 1
+              ) {
+                return a["price"] - b["price"];
+              } else if (
+                filterSort.parameter == "price" &&
+                filterSort.value == -1
+              ) {
+                return b["price"] - a["price"];
+              }
+            })
+            .map((e) => {
+              return (
+                <BookCard
+                  id={e.id}
+                  imageUrl={e.imageUrl}
+                  title={e.title}
+                  price={e.price}
+                ></BookCard>
+              );
+            })
+
+          /* 
             Iterate over books that you get from network
             populate a <BookCard /> component
             pass down books id, imageUrl, title, price and anything else that you want to 
             show in books Card.
-        */}
-
-        {books.map((e) => {
-          return (
-            <BookCard
-              id={e.id}
-              imageUrl={e.imageUrl}
-              title={e.title}
-              price={e.price}
-            />
-          );
-        })}
+        */
+        }
       </Main>
     </div>
   );
--- src/Components/SortAndFilterButtons/SortAndFilterButtons.jsx
+++ src/Components/SortAndFilterButtons/SortAndFilterButtons.jsx
@@ -1,80 +1,22 @@
-import { useState } from "react";
-import { useEffect } from "react";
-import axios from "axios";
-
 export const SortAndFilterButtons = ({ handleSort }) => {
-  const [books, setgetBooks] = useState([]);
-  // get all books when user lands on the page
-  // populate them as mentioned below
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  const getData = () => {
-    axios.get("http://localhost:8080/books").then((res) => {
-      setgetBooks(res.data);
-    });
-  };
-  // console.log(books);
-
-  const sortByTitleAsc = () => {
-    books.sort((a, b) => {
-      return a.title - b.title;
-    });
-    setgetBooks(books);
-  };
-
-  const sortByTitleDesc = () => {
-    books.sort((b, a) => {
-      return b.title - a.title;
-    });
-    setgetBooks(books);
-  };
-
-  const sortByPriceAsc = () => {
-    books.sort((a, b) => {
-      return a.price - b.price;
-    });
-    setgetBooks(books);
-  };
-  const sortByPriceDesc = () => {
-    books.sort((a, b) => {
-      return b.price - a.price;
-    });
-    setgetBooks(books);
-  };
+  
   return (
     <div className="sortButtons">
       {/*
         Create 4 sorting buttons here to sort by following criteria:
-
         sort title in Ascending order  class: sortByTitleAsc
         sort title in Descending order class: sortByTitleDesc
         sort price asending order      class: sortByPriceAsc
         sort price descending order    class: sortByPriceDesc
-
         on every button click, call the reusable sorting function
         you received from Parent component, 
         and sort the data.
-
       */}
 
-      <button className="sortByTitleAsc" onClick={sortByTitleAsc}>
-        {" "}
-        sortByTitleAsc{" "}
-      </button>
-      <button className="sortByTitleDesc" onClick={sortByTitleDesc}>
-        sortByTitleDesc{" "}
-      </button>
-      <button className="sortByPriceAsc" onClick={sortByPriceAsc}>
-        {" "}
-        sortByPriceAsc{" "}
-      </button>
-      <button className="sortByPriceDesc" onClick={sortByPriceDesc}>
-        {" "}
-        sortByPriceDesc{" "}
-      </button>
+      <button className="sortByTitleAsc" onClick={()=>{handleSort("title",1)}}>title Ascending order </button>
+      <button className="sortByTitleDesc" onClick={()=>{handleSort("title",-1)}}>title Descending order</button>
+      <button className="sortByPriceAsc" onClick={()=>{handleSort("price",1)}}>price asending order</button>
+      <button className="sortByPriceDesc" onClick={()=>{handleSort("price",-1)}}>price descending order</button>
     </div>
   );
 };
